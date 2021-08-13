75e15d14a9400ca32dec60d3df1e3ce308e7913d
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-08-13 20:28:58 +0530

    Integrate segmentation (#5)
    * setup_booting_os
    
    * implement_with_c
    
    * implementing_with_c
    
    * play with outputs
    
    * integrate_segmentation

--- kmain.c
+++ kmain.c
@@ -1,14 +1,15 @@
 #include "frame_buffer.h"
 #include "serial_port.h"
-
+#include "memory_seg.h"
 
 
     int main(){
 
-           char ptr2[] = "Hello Thushara Samaraweera";
+           char ptr2[] = "welcome YmeOS";
 
 
-    serial_write(0x3F8, ptr2, 26);
-    fb_write(ptr2, 26);
+    serial_write(0x3F8, ptr2, 13);
+    fb_write(ptr2, 13);
+    segments_install_gdt();
 
-    }
+}
